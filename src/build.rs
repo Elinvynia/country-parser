1c6e0e653bc96d45ffaba454a81abdaea0d19870
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2021-04-07 16:02:44 +0200

    Improve speed

---
+++ src/build.rs
@@ -0,0 +1,64 @@
+use serde::Deserialize;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+#[derive(Deserialize)]
+struct Country {
+    official_name: String,
+    short_name: String,
+    iso2: String,
+    iso3: String,
+    num3: u16,
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=Cargo.lock");
+    println!("cargo:rerun-if-changed=data.ron");
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("data.rs");
+
+    let in_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let in_path = Path::new(&in_dir).join("src").join("data.ron");
+    let countries: Vec<Country> = ron::from_str(&fs::read_to_string(in_path).unwrap()).unwrap();
+
+    let mut s = String::new();
+    for country in countries {
+        s.push_str(&format!("Country {{ official_name: String::from(\"{}\"), short_name: String::from(\"{}\"), iso2: String::from(\"{}\"), iso3: String::from(\"{}\"), num3: {} }}, ",
+            country.official_name, country.short_name, country.iso2, country.iso3, country.num3));
+    }
+
+    fs::write(
+        &dest_path,
+        format!(
+            "
+use serde::{{Serialize, Deserialize}};
+
+/// Represents a country and holds various information about it.
+#[non_exhaustive]
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
+pub struct Country {{
+    /// The 'official' name of the country, or at least as close as it gets.
+    pub official_name: String,
+    /// The commonly used short form of the country name.
+    pub short_name: String,
+    /// ISO 3166-1 alpha-2 code of the country.
+    pub iso2: String,
+    /// ISO 3166-1 alpha-3 code of the country.
+    pub iso3: String,
+    /// ISO 3166-1 numeric-3 code of the country.
+    pub num3: u16,
+}}
+
+lazy_static! {{
+    static ref DATA: Vec<Country> = vec![{}];
+}}
+
+",
+            s
+        ),
+    )
+    .unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,49 +16,30 @@
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
 
-use serde::{Deserialize, Serialize};
+#[macro_use]
+extern crate lazy_static;
 
-const DATA: &str = include_str!("data.ron");
-
-/// Represents a country and holds various information about it.
-#[non_exhaustive]
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Country {
-    /// The "official" name of the country, or at least as close as it gets.
-    pub official_name: String,
-    /// The commonly used short form of the country name.
-    pub short_name: String,
-    /// ISO 3166-1 alpha-2 code of the country.
-    pub iso2: String,
-    /// ISO 3166-1 alpha-3 code of the country.
-    pub iso3: String,
-    /// ISO 3166-1 numeric-3 code of the country.
-    pub num3: u16,
-}
+include!(concat!(env!("OUT_DIR"), "/data.rs"));
 
 /// Attempts to parse the input into a valid country, checking all possible fields.
 pub fn parse<T: AsRef<str>>(info: T) -> Option<Country> {
     let info = info.as_ref().to_lowercase();
+    let countries: &Vec<Country> = &*DATA;
 
-    // Guaranteed to not panic.
-    let countries: Vec<Country> = ron::from_str(DATA).unwrap();
-
-    countries.into_iter().find(|c| {
-        info == c.official_name.to_lowercase()
-            || info == c.short_name.to_lowercase()
-            || info == c.iso2.to_lowercase()
-            || info == c.iso3.to_lowercase()
-    })
+    countries
+        .iter()
+        .find(|c| {
+            info == c.official_name.to_lowercase()
+                || info == c.short_name.to_lowercase()
+                || info == c.iso2.to_lowercase()
+                || info == c.iso3.to_lowercase()
+        })
+        .cloned()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_parse() {
-        let _: Vec<Country> = ron::from_str(DATA).unwrap();
-    }
 
     #[test]
     fn test_country() {
